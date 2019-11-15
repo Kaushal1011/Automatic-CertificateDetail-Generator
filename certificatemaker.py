196e4aaab27c57fed021445ccecde2b93d1005e0
NamitS27 <namitshah2711@gmail.com>
2019-11-15 13:06:41 +0530

    Can change any font

--- certificatemaker.py
+++ certificatemaker.py
@@ -1,7 +1,7 @@
 """Script to add text on certificates using data in csv file on a png"""
 
-import cv2
 import csv
+from PIL import Image, ImageDraw, ImageFont
 
 
 def main():
@@ -15,18 +15,19 @@ def main():
 
         i = 1
         for row in data:
-            img = cv2.imread(filename)
-            # Add Coordinates of image where left bottom  of first character of text will be placed
-            # Coordinates can be found using simple software such as paint
-            cv2.putText(img, row[0], (2458, 1260),
-                        cv2.FONT_HERSHEY_SIMPLEX, 2.6, (0, 0, 0), 6)
-            cv2.putText(img, row[1], (1530, 1413),
-                        cv2.FONT_HERSHEY_SIMPLEX, 2.6, (0, 0, 0), 6)
-            cv2.imwrite("Certificates/"+str(i)+row[0]
-                        + ".png", img)
-            print("Running " + str(i) + " Images Processed")
-            i += 1
+            img = Image.open(filename)
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype('LIB_IT.ttf', 90) # write the path of the font which you want to select
+            draw.text((1370, 1030), row[0], (0, 0, 0), font=font)
+            """
+            first parameter is the location in pixel at which you want to write the text.
+            second parameter is the text you want to write.
+            if you want to add extra paramter change the .csv as per you requirement and write this code again.
+            """
+            img.save("Certificates/" + row[0] + ".png")
+            print(str(i) + " Images Processed")
 
+            i += 1
 
 if __name__ == "__main__":
     main()
